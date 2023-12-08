b3dbae8831110a07e7e4a849b3ebd5e4d44fba98
Tiffanie Orsoni <115878825+Cadilith@users.noreply.github.com>
2023-12-08 12:28:49 +0100

    Add files via upload

--- advent-of-code-D1.js
+++ advent-of-code-D1.js
@@ -2,10 +2,24 @@
 const fs = require('fs');
 const data = fs.readFileSync('input.txt', 'utf8');
 const lines = data.split(/\n/);
+const dic = {
+	one: 'o1e',
+	two: 't2o',
+	three: 't3e',
+	four: 'f4r',
+	five: 'f5e',
+	six: 's6x',
+	seven: 's7n',
+	eight: 'e8t',
+	nine: 'n9e',
+};
 
 function getNumbers(arr) {
 	let arrayOfNumbers = [];
 	arr.forEach((line) => {
+		Object.keys(dic).forEach((key) => {
+			line = line.replaceAll(key, dic[key]);
+		});
 		arrayOfNumbers.push(
 			Number(line.replace(/\D/g, '').slice(0, 1) + line.replace(/\D/g, '').slice(-1))
 		);
